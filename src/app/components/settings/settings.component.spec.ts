88121fff15c22ba9d64e957d39b9ef04a310ef1e
Federico Moreno <fedemoreno14@gmail.com>
2021-03-08 01:25:00 -0300

    settings unit test

--- src/app/components/settings/settings.component.spec.ts
+++ src/app/components/settings/settings.component.spec.ts
@@ -1,9 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SettingsComponent } from './settings.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SettingsStateService } from '../../services/settings-state.service';
+import { HARD_SETTINGS } from '../../consts/predifined-settings.const';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
@@ -13,7 +13,12 @@ describe('SettingsComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [SettingsComponent],
       imports: [RouterTestingModule, ReactiveFormsModule],
-      providers: [SettingsStateService]
+      providers: [{
+        provide: SettingsStateService,
+        useValue: {
+          state: HARD_SETTINGS
+        }
+      }]
     })
       .compileComponents();
   });
@@ -24,7 +29,10 @@ describe('SettingsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should check the form is initialized with the store settings', () => {
+    const hostElement = fixture.nativeElement;
+    expect(hostElement.querySelector('#boardWidth').value).toEqual(HARD_SETTINGS.boardWidth.toString());
+    expect(hostElement.querySelector('#boardHeight').value).toEqual(HARD_SETTINGS.boardHeight.toString());
+    expect(hostElement.querySelector('#minesQuantity').value).toEqual(HARD_SETTINGS.minesQuantity.toString());
   });
 });
